0e1c28644e40eaf95de3cbf0b7e4dd83ac7c37a3
root <root@localhost.localdomain>
2016-10-13 13:47:17 -0400

    first commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+first =input('a: ')
+sec =input('b: ')
+thi = input('c: ')
+four =int(first) + int(sec) +int(thi)
+five ="2"
+sic = int(four) / int(five)
+sev = "0.5"
+eig = int(sic) ** int(sev)
+print("the area of triangle is", eig)
